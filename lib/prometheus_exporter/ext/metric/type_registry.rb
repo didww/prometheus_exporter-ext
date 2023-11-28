79a313868ae86d6523fff4677c3e2fe93ed6d354
Denis Talakevich <senid231@gmail.com>
2023-11-28 16:34:05 +0200

    make stats collector a module

---
+++ lib/prometheus_exporter/ext/metric/type_registry.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'prometheus_exporter/metric'
+require 'prometheus_exporter/ext/metric/gauge_with_time'
+
+module PrometheusExporter::Ext::Metric
+  module TypeRegistry
+    class << self
+      attr_accessor :types
+    end
+
+    self.types = {}
+
+    module_function
+
+    def register(type, klass)
+      types[type.to_s] = klass
+    end
+
+    def find_metric_class(type)
+      klass = types[type.to_s]
+      raise ArgumentError, "Unknown metric type #{type}" unless klass
+
+      klass
+    end
+
+    register :gauge_with_time, PrometheusExporter::Ext::Metric::GaugeWithTime
+    register :counter, PrometheusExporter::Metric::Counter
+    register :gauge, PrometheusExporter::Metric::Gauge
+    register :summary, PrometheusExporter::Metric::Summary
+    register :histogram, PrometheusExporter::Metric::Histogram
+  end
+end
